47bb918ef8bc4b61ef0b99d1c425b44663122dbb
jamesthomasmoran <moranja999@gmail.com>
2020-12-23 15:02:32 +0000

    reduced number of comparisons

--- src/java/runner/GAScenario2Runner.java
+++ src/java/runner/GAScenario2Runner.java
@@ -2,9 +2,9 @@ package runner;
 
 public class GAScenario2Runner extends GABaseRunner{
 	//Number of times we will change bestValues
-	private static final int ITERATIONS = 20;
+	private static final int ITERATIONS = 5;
 	// number of times each group of values is run by algorithm to generate average
-	private static final int NUM_RUNS = 10;
+	private static final int NUM_RUNS = 5;
 
 	private static final int POP_SIZE = 1000;
 	private static final int NUM_ITER = 1000;
@@ -12,9 +12,6 @@ public class GAScenario2Runner extends GABaseRunner{
 	public static void main(final String[] args)
 	{
 		GAScenario2Runner runner = new GAScenario2Runner();
-
-		runner.runGAAlgorithm(ITERATIONS, NUM_RUNS, POP_SIZE, NUM_ITER,
-				runner.mutator, runner.singlePointCrossover);
 
 		runner.runGAAlgorithm(ITERATIONS, NUM_RUNS, POP_SIZE, NUM_ITER,
 				runner.mutator, runner.singlePointCrossover, runner.meanAlterer);
--- src/java/runner/PSOScenario2Runner.java
+++ src/java/runner/PSOScenario2Runner.java
@@ -3,9 +3,9 @@ package runner;
 public class PSOScenario2Runner extends PSOBaseRunner
 {
 	//Number of times we will change bestValues
-	private static final int ITERATIONS = 20;
+	private static final int ITERATIONS = 5;
 	// number of times each group of values is run by algorithm to generate average
-	private static final int NUM_RUNS = 10;
+	private static final int NUM_RUNS = 5;
 
 	private static final int POP_SIZE = 1000;
 	private static final int NUM_ITER = 1000;
